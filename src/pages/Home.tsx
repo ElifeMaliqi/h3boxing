bf081057a29427a27c71c382b9339ce06a8f6a77
ElifeMaliqi <157223381+ElifeMaliqi@users.noreply.github.com>
2025-09-02 16:50:45 +0200

    Reduce hero section height by 100px

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -63,7 +63,7 @@ const Home = () => {
       </div>
 
       {/* Hero Section */}
-      <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
+      <section className="relative h-[calc(100vh-100px)] flex items-center justify-center overflow-hidden">
         {/* Futuristic Grid Background */}
         <div className="absolute inset-0">
           <div className="absolute inset-0 bg-gradient-to-br from-black via-gray-900 to-black"></div>
